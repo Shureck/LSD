0017b9fa11250b7713f0f433a6c2d13db22eea58
Shureck <Sashalev200149@gmail.com>
2020-11-28 22:10:20 +0300

    MVP

--- app/src/main/java/com/example/lsd/SurveyActivity.java
+++ app/src/main/java/com/example/lsd/SurveyActivity.java
@@ -3,12 +3,30 @@ package com.example.lsd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SurveyActivity extends AppCompatActivity {
+
+    ArrayList<Product> products = new ArrayList();
+    ListView productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
+
+        if(products.size()==0){
+            products.add(new Product("Картофель"));
+            products.add(new Product("Чай"));
+            products.add(new Product("Яйца"));
+            products.add(new Product("Молоко"));
+            products.add(new Product("Макароны"));
+        }
+        productList = (ListView) findViewById(R.id.list);
+        Adapter adapter = new Adapter(this, R.layout.listitem, products);
+        productList.setAdapter(adapter);
+
     }
 }
